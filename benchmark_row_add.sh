d36ddfef42e422516865240aafbdb108f1012130
Stephen Polcyn <stephen.polcyn@gmail.com>
2019-01-12 13:00:24 -0500

    Updated benchmark script

--- benchmark_row_add.sh
+++ benchmark_row_add.sh
@@ -1,2 +1,8 @@
-./gencsv
+# benchmark_row_add.sh
+# Author: Stephen Polcyn
+
+if [ ! -f data.csv ]; then
+    ./gencsv
+fi
+
 timescaledb-parallel-copy --db-name=tutorial --table=challenge --verbose --reporting-period=1s --file=data.csv
